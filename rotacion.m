b4cbfa3ed1491b9c2832cdd5a2577db2d2d2d856
mrjutsu <davidrosilloricardo@MacBook-Pro-de-David-Rosillo-Ricardo.local>
2013-11-02 20:15:32 -0500

    rotacion
    Added new function whose purpose is to rotate an image based on two
    points and the angle between them. NOT FUNCTIONAL YET.

---
+++ rotacion.m
@@ -0,0 +1,35 @@
+function p_rot = rotacion(p)
+
+[m,n] = size(p);
+mitad = round(n/2);
+
+punto1 = 255;
+punto2 = 255;
+
+coord1 = [];
+coord2 = [];
+cont = 0;
+intensidad = 0;
+
+while cont < m
+    if (intensidad ~= punto1)
+        for i1 = 1:m
+            for j1 = 1:n
+                intensidad = p(i1,j1);
+                if (intensidad == punto1)
+                    coord1 = [i1,j1];
+                end
+            end
+        end
+    else
+        for i1 = 1:m
+            for j1 = 1:n
+                intensidad = p(i1,j1);
+                if (intensidad == punto1)
+                    coord1 = [i1,j1];
+                end
+            end
+        end
+    end
+    cont = cont + 1;
+end
