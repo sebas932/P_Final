158e34d0456806d8ae2bb0d9964a30332821ebfc
Sebastian Amariles <sebas932@gmail.com>
2013-11-11 16:34:12 -0500

    improve interfaz  of web section

--- web_files/head.php
+++ web_files/head.php
@@ -2,17 +2,17 @@
 <html>
 <head>
 	<meta charset="utf-8">
+	<meta http-equiv="refresh" content="3">
 	<title>Base de datos de parqueadero</title>
 
-	<!-- Demo styling -->
-	 
+	
 	<!-- jQuery: required (tablesorter works with jQuery 1.2.3+) -->
 	<script src="bower_components/jquery/jquery.min.js"></script>
 
 	<!-- Pick a theme, load the plugin & initialize plugin -->
 	<link href="bower_components/tablesorter/css/theme.default.css" rel="stylesheet">
 	<script src="bower_components/tablesorter/js/jquery.tablesorter.min.js"></script>
-	<script src="bower_components/tablesorter/js/jquery.tablesorter.widgets.min.js"></script>
+	<!-- <script src="bower_components/tablesorter/js/jquery.tablesorter.widgets.min.js"></script> -->
 	<script>
 	$(function(){
 		$('table').tablesorter({
@@ -23,5 +23,6 @@
 		});
 	});
 	</script>
-
+	<!-- Demo styling -->
+	 <link href="css/style.css" rel="stylesheet">
 </head>
--- web_files/index.php
+++ web_files/index.php
@@ -23,7 +23,7 @@
 			 
 		
 <?php 	
-		$result = mysql_query("SELECT * FROM ingresos WHERE 1")or die("Error en: $busqueda: " . mysql_error());;
+		$result = mysql_query("SELECT * FROM ingresos WHERE 1 ORDER BY idingreso DESC ")or die("Error en: $busqueda: " . mysql_error());;
         while ($row = @mysql_fetch_assoc($result)){    
 		    echo '<tr><td>'.$row['idingreso'].'</td><td>'.$row['placa'].'</td><td>'.$row['horaEntrada'].'</td><td>'.$row['horaSalida'].'</td></tr>'; 
         }      
